c2ab87fea79766d40fe1d0c9741d8348cb7128ba
jsifontez <juan.sifontez.dev@gmail.com>
2022-02-16 22:27:22 -0400

    lesson 13: Public, private & readonly in classes

--- src/app.ts
+++ src/app.ts
@@ -18,18 +18,24 @@
 // ----------Clasess------------
 
 class Invoice {
-  // all the properties created below are public
-  // which means that when we create an instance of that class, we can access to all that properties
-  // we can change it if we want, after the initial instance of the class
-  client: string;
-  details: string;
-  amount: number;
+  /**
+   * all the properties created below are public
+   * which means that when we create an instance of that class, we can access to all that properties
+   * we can change it if we want, after the initial instance of the class
+   * to do that we use access modifiers like:
+   * 'public': the default value. We can access and modify the property outside the class
+   * 'private': We can access to the property inside the function but not outside. Like a 'Class.property'
+   * 'readonly': We can read the property from outside and inside but we can't change it
+   */
+  // readonly client: string;
+  // private details: string;
+  // public amount: number;
 
-  constructor (c:string, d: string, a: number) {
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
+  constructor (
+    readonly client: string,
+    private details: string,
+    public amount: number
+  ){}
 
   format () {
     return `${this.client} owes £${this.amount} form ${this.details}`
